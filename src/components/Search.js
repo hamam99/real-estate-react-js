48dcc9e34ff9941d7c3cd61bc3ad19a8b18a3bfc
Hamam <zero.saturnus@gmail.com>
2022-11-28 14:48:46 +0700

    update style

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,7 +13,7 @@ const Search = () => {
       <CountryDropDown />
       <PropertyDropDown />
       <PriceRangeDropdown />
-      <button className="bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px]  h-16 rounded-lg flex justify-center items-center text-white">
+      <button className="bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px]  h-16 rounded-lg flex justify-center items-center text-white text-lg">
         <RiSearch2Line />
       </button>
     </div>
